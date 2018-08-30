e7abcd034dcc87d9828443bbc8cf2a2254f51200
pfgherardini <federico.gherardini@gmail.com>
2018-08-30 14:30:39 -0700

    picking this up again

--- R/minimal_test.R
+++ R/minimal_test.R
@@ -1,15 +1,19 @@
 minimal_test <- function() {
+    library(citrus)
+    setwd("C:/Users/fgherardini/temp/standalone_citrus/data")
     tab <- read.table("Patient20_diseased_unstim.fcs.clustered.txt", header = T, sep = "\t", stringsAsFactors = F, check.names = F)
     metadata.tab <- read.table("samples_tab.txt", header = T, sep = "\t", stringsAsFactors = F, check.names = F)
     features.names <- c("FunctionalMarker1", "FunctionalMarker2", "popsize")
 
     metadata.tab <- metadata.tab[metadata.tab$condition == "unstim",]
-    kk <- scaffold:::calculate_cluster_features(tab, metadata.tab, features.names, "condition", "sample")
-    ff <- scaffold:::convert_to_citrus_featureset(kk, "sample")
-    scaffold:::run_citrus_analysis(ff, metadata.tab$label, "./", "sam")
+    kk <- grappolo::get_cluster_features(tab, metadata.tab, features.names, "condition", "sample", out.format = "table")
+
+    
+    ff <- convert_to_citrus_featureset(kk, "sample")
+    run_citrus_analysis(ff, metadata.tab$label, "./", "sam")
     
     message("#########################")
     
-    scaffold:::run_citrus_analysis(ff, metadata.tab$label, "./", "pamr")
+    run_citrus_analysis(ff, metadata.tab$label, "./", "pamr")
 
 }
---
+++ R/wrapper.R
@@ -0,0 +1,43 @@
+#' @export
+convert_to_citrus_featureset <- function(tab, endpoint.grouping) {
+  rnames <- do.call(paste, list(tab[, endpoint.grouping], sep = "_"))
+  cnames <- setdiff(colnames(tab), endpoint.grouping)
+  ret <- as.matrix(tab[, cnames])
+  row.names(ret) <- rnames
+  colnames(ret) <- cnames
+  
+  return(list(allFeatures = ret, nFolds = 1))
+  
+  return(ret)
+}
+
+#' @export
+run_citrus_analysis <- function(citrus.features, endpoint, working.directory, model.type) {
+  family <- NULL
+  
+  if(is.character(endpoint) || is.factor(endpoint)) {
+    family <- "classification"
+    endpoint <- as.factor(endpoint)
+    
+  }
+  else
+    family <- "continuous"
+  
+  citrus.res <- citrus::citrus.endpointRegress(
+    model.type,
+    citrus.foldFeatureSet = citrus.features,
+    labels = endpoint,
+    family = family
+  )
+  
+  plot(
+    citrus.res,
+    working.directory,
+    citrus.foldClustering = NULL,
+    citrus.foldFeatureSet = citrus.features,
+    citrus.combinedFCSSet = NULL,
+    "stratifyingFeatures"
+  )
+  return(citrus.res)
+  
+}

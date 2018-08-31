dfcecec4a2d7aaa2b36f4de3e2830a1818178d85
pfgherardini <federico.gherardini@gmail.com>
2018-08-31 13:09:54 -0700

    started working on science dataset test

--- R/minimal_test.R
+++ R/minimal_test.R
@@ -14,9 +14,34 @@ minimal_test <- function() {
     
     ff <- convert_to_citrus_featureset(kk, "sample")
     sam.model <- run_citrus_analysis(ff, metadata.tab$label, "./", "sam")
+    glmnet.model <- run_citrus_analysis(ff, metadata.tab$label, "./", "glmnet")
     
     message("#########################")
     
     pam.model <- run_citrus_analysis(ff, metadata.tab$label, "./", "pamr")
+    
+}
+
+minimal_test2 <- function() {
+    library(citrus)
+    setwd("C:/Users/fgherardini/temp/standalone_citrus/science")
+    tab <- read.table("all.pooled.clustered.txt", header = T, 
+                      sep = "\t", stringsAsFactors = F, check.names = F)
+    metadata.tab <- read.table("metadata_tab_citrus.txt", header = T, sep = "\t", stringsAsFactors = F, 
+                               check.names = F)
+    features.names <- c("popsize")
+    
+    metadata.tab <- metadata.tab[metadata.tab$tissue %in% c("blood", "bone_marrow"), ]
+    kk <- grappolo::get_cluster_features(tab, metadata.tab, features.names, predictors = "condition", 
+                                         endpoint.grouping = "file", out.format = "table")
+    
+    
+    ff <- convert_to_citrus_featureset(kk, "file")
+    sam.model <- run_citrus_analysis(ff, metadata.tab$tissue, "./", "sam")
+    glmnet.model <- run_citrus_analysis(ff, metadata.tab$tissue, "./", "glmnet")
+    
+    message("#########################")
+    
+    pam.model <- run_citrus_analysis(ff, metadata.tab$tissue, "./", "pamr")
     
 }

f602e074701df2ceab0b469700b8825fc709494b
pfgherardini <federico.gherardini@gmail.com>
2018-09-07 17:02:09 -0700

    continued working on analysis of science dataset

--- R/minimal_test.R
+++ R/minimal_test.R
@@ -44,4 +44,38 @@ minimal_test2 <- function() {
     
     pam.model <- run_citrus_analysis(ff, metadata.tab$tissue, "./", "pamr")
     
+    tab$pamr.cv1se <- 0
+    tab$pamr.cv1se[pam.model$differentialFeatures$cv.1se$clusters] <- 1
+    
+    
+    tab$pamr.constrained <- 0
+    tab$pamr.constrained[pam.model$differentialFeatures$cv.fdr.constrained$clusters] <- 1
+    
+    
+    sam.res <- get_signifcant_features_matrix(sam.model$finalModel$model)
+    scores <- select_score_by_cluster(sam.res,  "Score(d)", max)
+    tab[scores$cluster, "sam.qvalue"] <- scores$"q-value"
+    tab[scores$cluster, "sam.score"] <- scores$"Score(d)"
+    
+    
+    write.table(tab, "all.pooled.clustered.txt", row.names = F, col.names = T, sep = "\t", quote = F)
+    
+    #scaffold analysis 
+    
+    input.files <- list.files(pattern = "*.clustered.txt$", full.names = TRUE)
+    
+    col.names <- c("CD45.2", "Ly6G", "IgD", "CD11c", "F480", "CD3", "NKp46", "CD23", "CD34", "CD115", 
+                   "CD19", "120g8", "CD8", "Ly6C", "CD4", "CD11b", "CD27", "CD16_32", "SiglecF", "Foxp3", "B220", 
+                   "CD5", "FceR1a", "TCRgd", "CCR7", "Sca1", "CD49b", "cKit", "CD150", "CD25", "TCRb", "CD43", "CD64",
+                   "CD138", "CD103", "IgM", "CD44", "MHCII")
+    
+
+    landmarks.data <- vite::load_landmarks_from_dir("landmarks/", asinh.cofactor = 5, transform.data = T)
+   
+    vite::run_scaffold_analysis(input.files, ref.file = input.files[[1]], landmarks.data, col.names)
+    
+    
+    
+    
+    
 }
--- R/wrapper.R
+++ R/wrapper.R
@@ -25,7 +25,49 @@ run_citrus_analysis <- function(citrus.features, endpoint, working.directory, mo
         labels = endpoint, family = family)
     
     plot(citrus.res, working.directory, citrus.foldClustering = NULL, citrus.foldFeatureSet = citrus.features, 
-        citrus.combinedFCSSet = NULL, "stratifyingFeatures")
+        citrus.combinedFCSSet = NULL, c("stratifyingFeatures", "errorRate"))
     return(citrus.res)
     
+}
+
+#' @export
+get_signifcant_features_matrix <- function(sam.model) {
+    tabs.list <- sam.model$siggenes.table[c("genes.up", "genes.lo")]
+    
+    ret <- lapply(tabs.list, function(x) {
+        if(is.vector(x))
+            df <- data.frame(t(x), check.names = FALSE, stringsAsFactors = FALSE)
+        else
+            df <- data.frame(x, check.names = FALSE, stringsAsFactors = FALSE)
+        
+        df$"Gene Name" <- NULL
+        
+        temp <- sapply(setdiff(names(df), "Gene ID"), function(x) {as.numeric(df[, x])})
+        df <- data.frame(feature = df$"Gene ID", temp, check.names = FALSE, stringsAsFactors = FALSE)
+        return(df)
+    })
+    
+    ret <- do.call(rbind, ret)
+    row.names(ret) <- NULL
+    
+    names(ret) <- gsub("q-value\\(%\\)", "q-value", names(ret))
+    ret[, "q-value"] <- ret[, "q-value"] / 100
+    
+    return(ret)    
+}
+
+#' @export
+select_score_by_cluster <- function(tab, col.name, f, use.abs = TRUE) {
+    cl <- sapply(strsplit(tab$feature, "_"), "[", 2)
+    tab$cluster <- cl
+    
+    tab <- plyr::ddply(tab, ~cluster, function(x) {
+        v <- x[, col.name]
+        if(use.abs)
+            v <- abs(v)
+        w <- which(v == f(v))
+        return(x[w, ])
+        
+    })
+    return(tab)
 }
